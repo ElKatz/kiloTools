bcf2d5da5bb70afe08d0f9a376417e80634ecf54
DESKTOP-KEJGC64\Admin <leor.katz@gmail.com>
2020-01-13 15:42:19 -0500

    updated sp2su to mirror changes of isi violation rate and then some

--- sp2su.m
+++ sp2su.m
@@ -21,35 +21,39 @@ su  = struct;
 
 for iS = 1:nSus
     fprintf('unit %0.0f\r', iS)
-    su(iS).clusterId    = sp.clusterId(iS);
-    spikeIdx            = sp.spikeClusters == sp.clusterId(iS);
-    su(iS).times        = sp.spikeTimesSecs(spikeIdx);
-    su(iS).clusterScore = sp.clusterScore(iS);
-    su(iS).uQ           = sp.uQ(iS);
-    su(iS).cR           = sp.cR(iS);
-    su(iS).isiV         = sp.isiV(iS);
+    su(iS).clusterId        = sp.clusterId(iS);
+    spikeIdx                = sp.spikeClusters == sp.clusterId(iS);
+    su(iS).times            = sp.spikeTimesSecs(spikeIdx);
+    su(iS).clusterScore     = sp.clusterScore(iS);
+    su(iS).uQ               = sp.uQ(iS);
+    su(iS).cR               = sp.cR(iS);
+    su(iS).sp.isiV_fpRate   = sp.sp.isiV_fpRate(iS);
+    su(iS).sp.isiV_rate     = sp.sp.isiV_rate(iS);
     if ~isempty(sp.wf)
         su(iS).wf           = squeeze(sp.wf(:, :, spikeIdx));
     else
         su(iS).wf           = [];
     end
-    su(iS).medWfPeakCh      = sp.medWfPeakCh(iS);
+    su(iS).peakCh           = sp.peakCh(iS);
     su(iS).medWfOnPeakCh    = sp.medWfOnPeakCh(iS,:);
 %     su(iS).medWfPerChannel  = squeeze(sp.medWfs(iS,:,:))';
 %     medWfAmpPerChannel      = max(su(iS).medWfPerChannel) - min(su(iS).medWfPerChannel);
 %     su(iS).medWfPeakChannel             = find(medWfAmpPerChannel == max(medWfAmpPerChannel), 1);
 %     su(iS).medWfOnPeak     = su(iS).medWfPerChannel(:, su(iS).medWfPeakChannel);
 % %     su(iS).medWfOnPeak     = median(squeeze(su(iS).wf(peakChannel, :, :)),2);
-    su(iS).info.Fs         = sp.sample_rate;
-    su(iS).info.meta       = 'kiloSort';
-    su(iS).info.dsn        = sp.info.ksDir;
-%     su(iS).info.dsFolder   = dsFolder; 
+    [a,b,c]             = fileparts(sp.info.ksDir);
+    su(iS).info.dsn     = b;
+    su(iS).info.ksDir   = ksDir; 
+    su(iS).info.Fs      = sp.sample_rate;
+    su(iS).info.meta    = 'kiloSort';
 end
 
 disp('Done!')
 
 %% save su
-
-save(fullfile(ksDir, 'su.mat'), 'su')
-
-disp('Done saving ''su''')
+if exist('ksDir', 'var')
+    save(fullfile(ksDir, 'su.mat'), 'su')
+    disp('Done saving ''su''')
+else
+    disp('no ''ksDir'' provided as input so I''m not saving nada')
+end
